7f5b8726d18f892ade20f7a9f7d1f6b8b3df1ea6
Sweta <ydvsweta19@gmail.com>
2026-05-13 00:46:21 +0530

    feat: update API base URL to use environment variable for flexibility

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://skillsync-a2t0.onrender.com'],
     credentials: true
 }))
 // using all the routes
